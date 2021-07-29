f0aa4983d2594f9bee5eb67322992f37f7c281a5
sophieqguan <sophie.qin.guan@gmail.com>
2021-07-28 19:57:56 -0700

    changed layout for different screen sizes , added loading bar

--- src/App.js
+++ src/App.js
@@ -4,26 +4,42 @@ import React,{ Component, useState} from "react";
 
 function App() {
   const [state, setState] = useState(null);
-
+  const apiURL = 'http://localhost:8080/upload';
   var imgFile = "";
 
-  const submitFile = () => {
-    if (state != null) {
-      var button = document.getElementById("submitBtn");
-      button.innerHTML = "<p class='text-muted' style={{color: 'white'}}>image submitted! now comes the wait...</p>";
-    }
-  }
-
   const changeGridDisplay = () => {
-    document.getElementById("imgHolder").classList = 'col-sm-6';
+    document.getElementById("imgHolder").classList = 'col-m-6 col-sm-6 col-xs-12';
   }
-
+  
   const updateImage = () => {
     var imgDisplay = document.getElementById("img");
     imgDisplay.src = imgFile;
   }
+  
+  const showLoading = () => {
+    var container = document.getElementById("loadingBar");
+    container.innerHTML = "<div class='progress'><div class='progress-bar bg-warning' id='progBar' role='progressbar' style=\"width:0%; background-color:#f0ad4e\"></div></div>";
+    //update percentage completed
+    setTimeout(function () {
+      updateLoadingPct(100);
+      setTimeout(function() {hideLoading()}, 1000)},
+      500);
+      //setTimeout(updateLoadingPct(100), 10000);
+  }
+
+  const hideLoading = () => {
+    var container = document.getElementById("loadingBar");
+    container.innerHTML = "";
+  }
+
+  const updateLoadingPct = (pct) => {
+    var container = document.getElementById("progBar");
+    var newStyle = "width:" + pct + "%; background-color: #f0ad4e";
+    container.style = newStyle;
+  }
 
   async function onFileChangeHandler (e) {
+    showLoading();
     changeGridDisplay();
     const [file] = e.target.files;
     imgFile = URL.createObjectURL(file);
@@ -37,53 +53,48 @@ function App() {
     const formData = new FormData();
     formData.append('file', file);
     
-    let res = await fetch ('http://localhost:8080/upload', {
+    let res = await fetch (apiURL, {
         method: 'post',
         body: formData
     });
-    if(res.ok) {
-        alert("File uploaded successfully.")
-        
-        const reader = await res.body.getReader();
 
-          let chunks = [];
-          reader.read().then(function processText({ done, value }) {
+    setTimeout(function () {displayFinal(res)}, 1000);
+    
+  }
 
-              if (done) {
-                  // console.log('Stream finished. Content received:')
+  async function displayFinal (res) {
+    if(res.ok) {      
+      const reader = await res.body.getReader();
+        let chunks = [];
+        reader.read().then(function processText({ done, value }) {
 
-                  // console.log(chunks);
+            if (done) {
+                const blob = new Blob([chunks], { type: "image/png" });
+                imgFile = URL.createObjectURL(blob);
+                updateImage();
+                return;
+            }
+            const tempArray = new Uint8Array(chunks.length + value.length);
+            tempArray.set(chunks);
+            tempArray.set(value, chunks.length);
+            chunks = tempArray
 
-                  const blob = new Blob([chunks], { type: "image/png" });
-                  // console.log(blob);
+            return reader.read().then(processText)
+        })
 
-                  imgFile = URL.createObjectURL(blob);
-                  updateImage();
-                  return;
-              }
-
-              // console.log(`Received ${chunks.length} chars so far!`)
-              // console.log(value);
-              const tempArray = new Uint8Array(chunks.length + value.length);
-              tempArray.set(chunks);
-              tempArray.set(value, chunks.length);
-              chunks = tempArray
-
-              return reader.read().then(processText)
-          })
-
-    };
-    console.log(res.status);
+  };
+  console.log(res.status);
   }
 
   return (
     <div className="App">
       <h1 class="App-logo float-start" style={{color:"white"}}>|</h1>
-      <h1 class="title float-end">Passport Photo Formatter</h1>
+      <h1 id="P3" style={{display:"none"}}>P3</h1>
+      <h1 id="appTitle" class="title float-end">Passport Photo Formatter</h1>
       <div class="container">
           <div class="row" style={{borderRadius:5}}>
             <div id="imgHolder" class = "col-sm-3 col-md-3 col-xs-3">
-                  <div class = "border border-warning" 
+                  <div id="imgLeft" class = "border border-warning" 
                       style={{borderRadius: 5, 
                               border:5,
                               borderColor: "pink",
@@ -91,19 +102,19 @@ function App() {
                       <img id = "img" src = "" 
                         class="img-fluid"
                         style={{width:"100%"}}/>
-                      {/*<img ref = {uploadedImage} 
-                      class="img-fluid"
-                      style={{width:"100%"}}/>*/}
                   </div>
             </div>
 
-            <div class = "col-md-6 col-sm-6 col-xs-6">
+            <div class = "col-md-6 col-sm-6 col-xs-12">
+            
+              <div id="loadingBar">
+              </div>
               <div class="text-white" style={{borderRadius: 5, backgroundColor: "#454545"}}>
                 <div class="" style={{padding:10}}>
-                  <div class="card-header text-warning">Upload an image</div>
+                  <div id="cardTitle" class="card-header text-warning">Upload an image</div>
                   
                   <div class="card-body text-white">
-                      <p class="card-text" style={{padding:15, color: "white", fontSize: "10px"}}>
+                      <p id="instruction" class="card-text" style={{padding:15, color: "white", fontSize: "10px"}}>
                         import an image and we'll place it on 
                         a standard passport photo grid for you, 
                         which you can save and use to print.
@@ -113,16 +124,8 @@ function App() {
                     <input type="file" className="form-control input-color" name="file" 
                     onChange={onFileChangeHandler}/>
                   </div>  
-
-                  <div id = "submitBtn">
-                    <button type="button" class="btn btn-warning btn-block" onClick={submitFile}>Go!</button>
-                  </div>
-
               </div>
             </div>
-          
-            
-
           </div>
         </div>
       </div>
